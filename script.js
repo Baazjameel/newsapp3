dc4f0124b07d84d0f58a08af4e4817125fcb6dd7
Baazjameel <129052832+Baazjameel@users.noreply.github.com>
2023-04-12 19:50:16 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,26 @@ $(document).ready(function() {
     event.preventDefault();
     var category = $('#category').val();
     $.ajax({
-      url: 'get_articles.php',
+      url: '$(document).ready(function() {
+  $('#category-form').submit(function(event) {
+    event.preventDefault();
+    var category = $('#category').val();
+    $.ajax({
+      url: 'http://localhost/get_articles',
+      type: 'POST',
+      data: { category: category },
+      dataType: 'json',
+      success: function(data) {
+        var results = $('#results');
+        results.empty();
+        for (var i = 0; i < data.length; i++) {
+          results.append('<h2>' + data[i].title + '</h2>');
+          results.append('<p><a href="' + data[i].url + '">' + data[i].url + '</a></p>');
+        }
+      }
+    });
+  });
+});',
       type: 'POST',
       data: { category: category },
       dataType: 'json',
